064899c84b8e0cdc5b3a6fa5dd8dc7805978a49b
mazmazz <mar.marcoz@outlook.com>
2018-01-15 01:32:30 -0500

    Support mflags and looping in MusicChange hook

--- src/lua_hook.h
+++ src/lua_hook.h
@@ -78,6 +78,6 @@ boolean LUAh_LinedefExecute(line_t *line, mobj_t *mo, sector_t *sector); // Hook
 boolean LUAh_PlayerMsg(int source, int target, int flags, char *msg); // Hook for chat messages
 boolean LUAh_HurtMsg(player_t *player, mobj_t *inflictor, mobj_t *source); // Hook for hurt messages
 #define LUAh_PlayerSpawn(player) LUAh_PlayerHook(player, hook_PlayerSpawn) // Hook for G_SpawnPlayer
-boolean LUAh_MusicChange(const char *oldname, const char *newname, char *newmusic); // Hook for music changes
+boolean LUAh_MusicChange(const char *oldname, const char *newname, char *newmusic, UINT16 *mflags, boolean *looping); // Hook for music changes
 
 #endif
