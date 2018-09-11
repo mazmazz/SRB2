688adc365179b4e46471c3dff15986fdc9c68cac
mazmazz <mar.marcoz@outlook.com>
2018-09-10 21:12:56 -0400

    Savegame fixes

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -831,7 +831,7 @@ static void P_NetUnArchiveWorld(void)
 		if (diff3 & SD_COLORMAP)
 		{
 			extracolormap_t *exc;
-			size_t dbg_i = 0;
+			//size_t dbg_i = 0;
 
 			UINT8 fadestart = READUINT8(get),
 				fadeend = READUINT8(get),
@@ -880,7 +880,7 @@ static void P_NetUnArchiveWorld(void)
 #ifdef EXTRACOLORMAPLUMPS
 				if (exc->lump != LUMPERROR)
 				{
-					dbg_i++;
+					//dbg_i++;
 					continue;
 				}
 #endif
@@ -890,17 +890,17 @@ static void P_NetUnArchiveWorld(void)
 					&& fadeend == exc->fadeend
 					&& fog == exc->fog)
 				{
-					CONS_Debug(DBG_RENDER, "P_NetUnArchiveWorld: Found map %d: rgba(%d,%d,%d,%d) fadergba(%d,%d,%d,%d)\n",
-						dbg_i, cr, cg, cb, ca, cfr, cfg, cfb, cfa);
+					// CONS_Debug(DBG_RENDER, "P_NetUnArchiveWorld: Found map %d: rgba(%d,%d,%d,%d) fadergba(%d,%d,%d,%d)\n",
+					// 	dbg_i, cr, cg, cb, ca, cfr, cfg, cfb, cfa);
 					break;
 				}
-				dbg_i++;
+				//dbg_i++;
 			}
 
 			if (!exc)
 			{
-				CONS_Debug(DBG_RENDER, "P_NetUnArchiveWorld: Creating map %d: rgba(%d,%d,%d,%d) fadergba(%d,%d,%d,%d)\n",
-					dbg_i, cr, cg, cb, ca, cfr, cfg, cfb, cfa);
+				// CONS_Debug(DBG_RENDER, "P_NetUnArchiveWorld: Creating map %d: rgba(%d,%d,%d,%d) fadergba(%d,%d,%d,%d)\n",
+				// 	dbg_i, cr, cg, cb, ca, cfr, cfg, cfb, cfa);
 
 				exc = Z_Calloc(sizeof (*exc), PU_LEVEL, NULL);
 
@@ -925,14 +925,16 @@ static void P_NetUnArchiveWorld(void)
 
 				R_AddColormapToList(exc);
 
-				sectors[i].extra_colormap = exc;
-
 #ifdef EXTRACOLORMAPLUMPS
 				exc->lump = LUMPERROR;
 				exc->lumpname[0] = 0;
-			} // if (!exc) // if (!lumpname[0] || !R_ColormapForName(lumpname))
 #endif
 			}
+
+			sectors[i].extra_colormap = exc;
+#ifdef EXTRACOLORMAPLUMPS
+			}
+#endif
 		}
 
 		if (diff & SD_FFLOORS)
