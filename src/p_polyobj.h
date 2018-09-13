7be843e8529fddd3e9d3a8dea22474f8d8b51748
mazmazz <mar.marcoz@outlook.com>
2018-09-13 00:04:50 -0400

    492 PolyObj Fade, 491 PolyObj Translucency, 490 PolyObj changes
    * 490: Set proper render flags according to spawnflags
    * 491: Add relative calc (EFFECT3) and Front X alpha param (DONTPEGTOP)
    * 492:
        * Tic-based (EFFECT4) and speed timing
        * Add relative calc (EFFECT3) and Front X alpha param (DONTPEGTOP)
        * Set proper render flags according to spawnflags
        * Fix OpenGL >= NUMTRANSMAPS render bug

--- src/p_polyobj.h
+++ src/p_polyobj.h
@@ -211,12 +211,13 @@ typedef struct polyfade_s
 	thinker_t thinker; // must be first
 
 	INT32 polyObjNum;
+	INT32 sourcevalue;
 	INT32 destvalue;
 	boolean docollision;
 	boolean doghostfade;
+	boolean ticbased;
 	INT32 timer;
 	INT32 speed;
-	UINT32 interval;
 } polyfade_t;
 
 //
@@ -284,9 +285,8 @@ typedef struct polyfadedata_s
 	INT32 destvalue;
 	boolean docollision;
 	boolean doghostfade;
-	INT32 timer;
+	boolean ticbased;
 	INT32 speed;
-	UINT32 interval;
 } polyfadedata_t;
 
 //
