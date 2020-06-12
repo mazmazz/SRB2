da4675e2a7c871d98d6a66728b7ac5d2f95b8b5f
mazmazz <mar.marcoz@outlook.com>
2020-06-12 14:52:01 -0400

    Music: De-couple soundfonts from HAVE_MIXERX

--- src/s_sound.c
+++ src/s_sound.c
@@ -307,9 +307,9 @@ void S_RegisterSoundStuff(void)
 #endif
 #ifdef HAVE_MIXERX
 	CV_RegisterVar(&cv_midiplayer);
-	CV_RegisterVar(&cv_midisoundfontpath);
 	CV_RegisterVar(&cv_miditimiditypath);
 #endif
+	CV_RegisterVar(&cv_midisoundfontpath);
 
 	COM_AddCommand("tunes", Command_Tunes_f);
 	COM_AddCommand("restartaudio", Command_RestartAudio_f);
